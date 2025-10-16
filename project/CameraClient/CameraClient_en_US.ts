b3f1e07918ea552014998aac2adc2051d2c74ed7
south <imtangsheng@outlook.com>
2025-10-16 16:01:05 +0800

    add资源提交

---
+++ project/CameraClient/CameraClient_en_US.ts
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US" sourcelanguage="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="27"/>
+        <source>English</source>
+        <translation>中文</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="40"/>
+        <source>中文应用</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>south::ShareLib</name>
+    <message>
+        <location filename="../../include/share_lib.h" line="106"/>
+        <source>设备模块未找到:%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../include/share_lib.h" line="127"/>
+        <source>最多支持 10 个参数</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../include/share_lib.h" line="151"/>
+        <source>发送的参数类型是不支持的参数类型</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../include/share_lib.h" line="179"/>
+        <source>方法调用失败,请检测模块%1的方法:%2使用的参数是否正确</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../include/share_lib.h" line="216"/>
+        <source>调用失败</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../include/share_lib.h" line="247"/>
+        <source>Share::Share() - Current thread:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
